5db56525d5fbd1f4a58dbba800ec5b9b2016bf11
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-02-05 16:42:43 +0100

    added bootstrap

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
+import '@fontsource/montserrat'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
-import '@fontsource/montserrat'
 
 createApp(App).mount('#app')
