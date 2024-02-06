0adeee4681c9b410089868e840a31209920863bb
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-02-06 17:09:27 +0100

    added color map

--- src/data/index.js
+++ src/data/index.js
@@ -19,4 +19,24 @@ const typePokemon = [
     "Water"
 ]
 
-export { typePokemon }
+const colorMap = {
+    "Bug": '#f8d5a3',
+    "Dark": '#044286',
+    "Electric": '#97b3e6',
+    "Fairy": '#fceaff',
+    "Fighting": '#E6E0D4',
+    "Fire": '#FDDFDF',
+    "Flying": '#F5F5F5',
+    "Ghost": '#663388',
+    "Grass": '#DEFDE0',
+    "Ground": '#f4e7da',
+    "Ice": '#DEF3FD',
+    "Normal": '#F5F5F5',
+    "Poison": '#98d7a5',
+    "Psychic": '#eaeda1',
+    "Rock": '#d5d5d4',
+    "Steel": '#gray',
+    "Water": '#DEF3FD'
+}
+
+export { typePokemon, colorMap }
